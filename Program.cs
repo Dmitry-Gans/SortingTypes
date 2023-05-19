6e9b08fcb23400cbe99a676b52f0ac259dc84ca9
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-05-19 22:54:54 +0300

    Добавлены два модуля и сорганизация труктуры проекта

--- Program.cs
+++ Program.cs
@@ -1,24 +1,10 @@
 ﻿// Сортировка выбором
-// Легкий вариант
+// Способ с использованием Классов, которые же сами и создали
 
-void SortSelection(int[] collection)
-{
-    int size = collection.Length;
+using static Sorting; // Класс сортировки
+using static Infrastructure; // Класс создания и показа массива
 
-    for (int i = 0; i < size - 1; i++)
-    {
-        int position = i; // Минимальная позиция
-        for (int j = i + 1; j < size; j++)
-        {
-            if (collection[j] < collection[position]) position = j;
-        }
-        int temp = collection[i];
-        collection[i] = collection[position];
-        collection[position] = temp;
-    }
-}
-int[] array = {7, 6, 3, 4, 5, 1, 2, 3};
-
-Console.WriteLine($"[{String.Join(", ", array)}]");
+int[] array = CreateArray(10);
+Show(array);
 SortSelection(array);
-Console.WriteLine($"[{String.Join(", ", array)}]");
+Show(array);
