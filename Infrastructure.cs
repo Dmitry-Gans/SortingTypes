44ee613e0ef1a87bd0c5275575d0054f9dd22c47
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-05-20 14:05:13 +0300

    Полноценный проект сортировки и оформления готов

--- Infrastructure.cs
+++ Infrastructure.cs
@@ -1,14 +1,17 @@
 public static class Infrastructure
 {
-    public static int[] CreateArray(int size, int min = 0, int max = 10)
+    public static int[] CreateArray(this int size, int min = 0, int max = 10)
     {
         return Enumerable.Range(1, size)
                 .Select(item => Random.Shared.Next(min, max))
                 .ToArray();
      }
-
-    public static void Show(int[] array)
+     
+    public static int[] Show(this int[] array, string separator = ",") // Можно так же добавить
+    //string, чтобы затем использловать как переменную, с дефолтным разделителем вместо привычного Join
     {
-        Console.WriteLine($"[{String.Join(", ", array)}]");
+        string output = string.Join(separator, array);
+        Console.WriteLine($"[{output}]");
+        return array;
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
 using static Sorting; // Класс сортировки
 using static Infrastructure; // Класс создания и показа массива
 
-int[] array = CreateArray(10);
-Show(array);
-SortSelection(array);
-Show(array);
+// int[] array = CreateArray(10);
+// Show(array);
+// SortSelection(array);
+// Show(array);
+
+10.CreateArray(min: 10, max: 20) // Теперь можем сами задавать значения через двоеточие
+  .Show("/") // Теперь можем задавать знак разделителя поместив его в ковычки 
+  .SortSelection()
+  .Show("-");
